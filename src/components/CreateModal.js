0eebe74326c66bd5aacc3553eb544729decbeb4a
Marcos8060 <marcosgav80@gmail.com>
2022-06-16 00:12:52 +0300

    added a task modal

--- src/components/CreateModal.js
+++ src/components/CreateModal.js
@@ -1,15 +1,33 @@
-import React from 'react'
-import '../assets/style.css'
-import { AiFillPlusCircle } from 'react-icons/ai'
+import React,{useState} from "react";
+import "../assets/style.css";
+import { AiFillPlusCircle } from "react-icons/ai";
+import Modal from 'react-bootstrap/Modal';
+
 
 const CreateModal = () => {
-  return (
-    <div>
-        <button className='crudBtn'>
-            Add a task <AiFillPlusCircle className='icon' />
-        </button>
-    </div>
-  )
-}
+    const [show, setShow] = useState(false);
 
-export default CreateModal
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+      <button className="crudBtn" onClick={handleShow}>
+        Add a task <AiFillPlusCircle className="icon" />
+      </button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add a Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="modalBody">
+            <form>
+               <input type="text" className="form-control" placeholder="Enter task..." />
+               <textarea name="" id="" cols="30" rows="4" className="form-control mt-4" placeholder="Give a description..."></textarea>
+               <button type="button" class="addBtn">Add Task</button>
+            </form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default CreateModal;
